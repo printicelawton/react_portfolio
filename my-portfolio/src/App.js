f2d012d436faeab66f48ecd117fc3c856eb98657
Printice Lawton <printicelaw10@gmail.com>
2021-07-14 14:53:02 -0400

    routes and pages'

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -6,7 +6,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
 
 import Footer from './components/Footer';
-// import HomePage from ' ./pages/HomePage'; 
+import HomePage from './pages/HomePage'; 
 // import AboutPage from ' ./pages/AboutPage';
 // import ContactPage from './pages/ContactPage'; 
 
@@ -53,9 +53,9 @@ class App extends React.Component {
             </Navbar.Collapse>
           </Navbar>
 
-          {/* <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
           <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
-          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} /> */}
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
           
           <Footer />
 
---
+++ my-portfolio/src/pages/HomePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HomePage(props) {
+
+}
+
+export default HomePage
