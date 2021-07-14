8feb17b82028ca5238ec07ff1614669846908d04
Printice Lawton <printicelaw10@gmail.com>
2021-07-14 19:45:23 -0400

    CardInfo Component done

--- my-portfolio/src/components/Card.js
+++ my-portfolio/src/components/Card.js
@@ -6,9 +6,12 @@ function Card(props) {
 
     return(
         <div className="d-inline-block g-card" onClick={(e) => props.click(props.item)}>
-            <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc}>
+            <img className="g-card-image" src={props.item.imgSrc} alt={props.item.imgSrc}/>
+            { props.item.selected && <CardInfo titel={props.item.title} subTitle={props.item.subTitle} link={props.item.link} />}
 
         </div>
 
     );
 }
+
+export default Card; 
