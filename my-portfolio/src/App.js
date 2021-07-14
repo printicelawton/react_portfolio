53b704cc5ebd2625585a3f22cf4bb3a1fef2059e
Printice Lawton <printicelaw10@gmail.com>
2021-07-14 12:18:31 -0400

    Navbar build

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,15 @@
 import React from 'react'; 
 import './App.css';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom'; 
+import Container from 'react-bootstrap/Container'; 
+import Navbar from 'react-bootstrap/Navbar'; 
+import Nav from 'react-bootstrap/Nav'
+
+import Footer from '.components/Footer';
+import HomePage from ' ./pages/HomePage'; 
+import AboutPage from ' ./pages/AboutPage';
+import ContactPage from './pages/ContactPage'; 
+
 
 class App extends React.Component {
   constructor(props) {
@@ -27,11 +37,32 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>Hello From React</div>
-      );
+      <Router>
+        <Container className="p-0" fluid={true}>
+          
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Printice Lawton</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
+          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
+          
+          <Footer />
+
+        </Container>
+      </Router>
+    );
   }
-  
-  
 }
 
 export default App;
